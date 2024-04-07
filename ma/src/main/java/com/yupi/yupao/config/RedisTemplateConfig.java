4f68481232faa6913b2ecb245ff4c018189a5fbb
yijiawenCoder <131642520+yijiawenCoder@users.noreply.github.com>
2024-04-07 20:21:42 +0800

    Add files via upload

---
+++ ma/src/main/java/com/yupi/yupao/config/RedisTemplateConfig.java
@@ -0,0 +1,22 @@
+package com.yupi.yupao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+
+@Configuration
+public class RedisTemplateConfig {
+
+    // 配置RedisTemplate，设置key和value的序列化方式
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        return redisTemplate;
+    }
+}
